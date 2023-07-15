023f08f118e83c5cfc2d4dd032afaae8a495a825
TakanoriHirakawa <136884342+TakanoriHirakawa@users.noreply.github.com>
2023-07-15 09:51:51 +0900

    Add files via upload

---
+++ Theme17/IntOutOfRange.java
@@ -0,0 +1,7 @@
+package review.kadai17;
+
+public class IntOutOfRange extends Exception {
+	public IntOutOfRange(String msg) {
+		super(msg);
+	}
+}
---
+++ Theme17/MSG_InputMismatchException.java
@@ -0,0 +1,9 @@
+package review.kadai17;
+
+import java.util.InputMismatchException;
+
+public class MSG_InputMismatchException extends  InputMismatchException {
+	public  MSG_InputMismatchException(String s) {
+		super(s);
+	}
+}
