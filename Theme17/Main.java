e5faac1dfc8fd77b1847c093c7756213614fdaa7
TakanoriHirakawa <136884342+TakanoriHirakawa@users.noreply.github.com>
2023-07-16 13:30:09 +0900

    update Main

--- Theme17/Main.java
+++ Theme17/Main.java
@@ -1,4 +1,5 @@
 package review.kadai17;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -6,17 +7,22 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("プログラムを起動します。");
 		System.out.print("数値データを入力してください。>");
-		try{
-			int i = new Scanner(System.in).nextInt();
-			if (i<0) {
-				throw new IntOutOfRange("入力されたデータが下限を下回っています。");
-			}else if(9<i) {
-				throw new IntOutOfRange("入力されたデータが上限を上回っています。");
+		try {
+			String inputStr = new Scanner(System.in).nextLine();
+			if (inputStr.matches("[0-9]+") == false) {
+				throw new MSG_InputMismatchException("半角数字１桁を入力してください。");
+			} else {
+				int number = Integer.parseInt(inputStr);
+				if (number < 0) {
+					throw new IntOutOfRange("入力されたデータが下限を下回っています。");
+				} else if (9 < number) {
+					throw new IntOutOfRange("入力されたデータが上限を上回っています。");
+				}
+				System.out.println("正しい数値データ" + number + "が入力されました");
 			}
-			System.out.println("正しい数値データ"+ i +"が入力されました");
-		}catch(InputMismatchException e){
+		} catch (InputMismatchException e) {
 			throw new MSG_InputMismatchException("半角数字１桁を入力してください。");
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
